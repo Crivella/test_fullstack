ecebfb0069ae86fa0c8a1d63db858e5208ccabf8
Crivella <davide.grassano@epfl.ch>
2023-05-07 23:30:10 +0200

    Implemented API functionalities as child wrappers

---
+++ todo/django_frontend_react2/src/API/AuthWrapper.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import PassPropsWrapper from '../components/Wrapper';
+
+const endpoint = process.env.REACT_APP_AUTH_ENDPOINT;
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.withCredentials = true
+
+
+export default function APIAuthWrapper({ children }) {
+    const [user, setUser] = useState("");
+
+    useEffect(() => {console.log(children); updateUser()}, []);
+
+    const updateUser = () => {
+        return axios.get(`${endpoint}/get-user/`, {
+            headers: { 'Content-Type': 'application/json' }
+            })
+            .then(({data}) => {setUser(data.username); return data.username})
+            .catch((err) => console.log(err));
+    };
+
+    const login = async (data) => {
+        await axios.post(`${endpoint}/login/`, data, {})
+            .catch((err) => console.log(err));
+        return updateUser();
+    };
+
+    const logout = async () => {
+        await axios.post(`${endpoint}/logout/`, {}, {})
+            .catch((err) => console.log(err));
+        return updateUser();
+    };
+
+    const newProps = {
+        'user': user,
+        'login': login,
+        'logout': logout,
+    }
+
+    return (
+        <PassPropsWrapper newProps={newProps}>
+            {children}
+        </PassPropsWrapper>
+    )
+}
---
+++ todo/django_frontend_react2/src/API/ListWrapper.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import PassPropsWrapper from '../components/Wrapper';
+
+const endpoint = process.env.REACT_APP_TODO_ENDPOINT;
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.withCredentials = true
+
+
+export default function APIListWrapper({ children, user }) {
+    const [list, setList] = useState([]); // [{}
+
+    useEffect(() => {getList()}, [user]);
+
+    const getList = () => {
+        return axios.get(`${endpoint}/`, {
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(({data}) => setList(data))
+        .catch((err) => console.log(err));
+    };
+
+    const getItem = (id) => {
+        return axios.get(`${endpoint}/${id}/`, {
+            headers: { 'Content-Type': 'application/json' }
+        }).then(({data}) => data);
+    };
+
+    const updateItem = (id, data) => {
+        return axios.patch(`${endpoint}/${id}/`, data, {})
+            .then(({data}) => setList(list.map((e) => e.id === id ? data : e)))
+            .catch((err) => console.log(err));
+    };
+
+    const deleteItem = (id) => {
+        axios.delete(`${endpoint}/${id}/`, {}) 
+            .then(() => setList(list.filter((e) => e.id !== id)))
+            .catch((err) => console.log(err));
+    };
+
+    const addItem = (data) => {
+        return axios.post(`${endpoint}/`, data, {})
+            .then(({data}) => setList([...list, data]))
+            .catch((err) => console.log(err));
+    };
+
+    const newProps = {
+        'list': list, // [{}, {}, {}]
+        'getList': getList,
+        'getItem': getItem,
+        'updateItem': updateItem,
+        'deleteItem': deleteItem,
+        'addItem': addItem,
+    }
+
+    return (
+        <PassPropsWrapper newProps={newProps}>
+            {children}
+        </PassPropsWrapper>
+    )
+}
