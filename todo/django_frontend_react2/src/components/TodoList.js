fe54ec279eca0051b65074630b2d424fd2aa6c05
Crivella <davide.grassano@epfl.ch>
2023-05-07 17:12:59 +0200

    Changed TodoList to use react-bootstrap

--- todo/django_frontend_react2/src/components/TodoList.js
+++ todo/django_frontend_react2/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
+import { ListGroup } from 'react-bootstrap';
 
 const endpoint = process.env.REACT_APP_TODO_ENDPOINT;
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
@@ -28,51 +29,35 @@ export default function TodoList({ user }) {
     setTodos([...todos, todo]);
   }
 
-  const updateTodo = (todo) => {
-    setTodos([...todos]);
+  const updateTodo = (todo, e) => {
+    axios.patch(`${endpoint}/api/todo/` + todo.id + "/", 
+      {...todo, completed: e.target.checked}
+    )
+      .then(() => todo.completed = !todo.completed)
+      .then(() => setTodos([...todos]))
+      .catch((e) => console.log('Cant edit todo while not logged in!'));
   }
 
+
   return (
-    <div className='container pt-3 bg-dark text-white'>
+    <ListGroup className='container pt-3 bg-dark text-white'>
       <h1>Todo List</h1>
       <ul className='list-group'>
-        <div className='list-group-item d-flex justify-content-between bg-primary text-white'>
-          <div>Titles</div>
-          <div>Completed</div>
-        </div>
-        {todos.map((e) => (
-          <Todo key={e.id} user={user} todo={e} updateTodo={updateTodo} />
+        <ListGroup.Item className='list-group-item d-flex justify-content-between bg-primary text-white'>
+          <span>Titles</span>
+          <span>Completed</span>
+        </ListGroup.Item>
+        {todos.map((todo) => (
+          // <Todo key={e.id} user={user} todo={e} updateTodo={updateTodo} />
+          <ListGroup.Item key={todo.id} className='list-group-item d-flex justify-content-between'>
+            <span>{todo.title}</span>
+            <input type='checkbox' checked={todo.completed} onChange={(e) => updateTodo(todo, e)}/>
+          </ListGroup.Item>
         ))}
       </ul>
       <input type='checkbox' onChange={TEST} />
       <AddForm user={user} addTodo={addTodo} />
-    </div>
-  );
-}
-
-function Todo({user, todo, updateTodo}) {
-  const [errCompleted, setErrCompleted] = useState(false) 
-  useEffect(() => {
-    // todo.title += "_test"
-    // updateTodo(todo);
-    // console.log(todo.id);
-    }, []);
-
-  const toggleCompleted = (e) => {
-    // if (! user) return;
-    axios.patch(`${endpoint}/api/todo/` + todo.id + "/", 
-      {...todo, completed: e.target.checked})
-    .then(() => todo.completed = !todo.completed)
-    .then(() => updateTodo())
-    .then(() => setErrCompleted(false))
-    .catch(() => setErrCompleted(true));
-  };
-
-  return (
-    <li className='list-group-item list-group-item-action d-flex justify-content-between'>
-      {todo.title}
-      <input type='checkbox' checked={todo.completed} onChange={toggleCompleted}/>
-    </li>
+    </ListGroup>
   );
 }
 
