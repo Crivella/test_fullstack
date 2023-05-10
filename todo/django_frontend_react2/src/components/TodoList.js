e82a53185b159df85ca9e5d122455649327d57a3
Crivella <davide.grassano@epfl.ch>
2023-05-11 01:13:14 +0200

    Implemented Grid/Form inside cards for alignment

--- todo/django_frontend_react2/src/components/TodoList.js
+++ todo/django_frontend_react2/src/components/TodoList.js
@@ -1,4 +1,4 @@
-import { Alert, Button, Card, Container, ListGroup } from 'react-bootstrap';
+import { Alert, Button, Card, Col, Container, Form, ListGroup, Row } from 'react-bootstrap';
 
 export default function TodoList({ active, setActive, list, updateItem, deleteItem, ...rest }) {
     const {theme, themeContrast1, themeContrast2} = rest;
@@ -33,8 +33,13 @@ export default function TodoList({ active, setActive, list, updateItem, deleteIt
                 {list.map((todo) => (
                     // <Todo key={e.id} user={user} todo={e} updateTodo={updateTodo} />
                     <ListGroup.Item as={Card} key={todo.id} bg={theme} text={themeContrast1} border={themeContrast2} className='mt-1'>
-                        <Card.Header className='d-flex justify-content-between' >
-                            <span onClick={() => onSelect(todo.id)}>{todo.priority}. {todo.title}</span>
+                        <Card.Header as={Form} className='d-flex justify-content-between' >
+                            <Form.Group as={Row} className='d-flex flex-grow-1' >
+                                <Col sm={3} md={2}>
+                                    <Form.Control type='number' value={todo.priority} />
+                                </Col>
+                                <Form.Label as={Col} sm={9} md={10} onClick={() => onSelect(todo.id)}> {todo.title}</Form.Label>
+                            </Form.Group>
                             <input style={{width: '2rem'}} type='checkbox' checked={todo.completed} onChange={(e) => onCheck(todo, e)}/>
                         </Card.Header>
                         <Card.Body as={Alert} show={active == todo.id} variant={themeContrast2}>
