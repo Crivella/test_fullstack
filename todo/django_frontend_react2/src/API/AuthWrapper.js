cdbf0298f764518c6fcd8edb9543a43c5b105043
Crivella <davidecrivella@yahoo.it>
2023-05-17 22:05:25 +0200

    Added GET toward /login to get CSRF token

--- todo/django_frontend_react2/src/API/AuthWrapper.js
+++ todo/django_frontend_react2/src/API/AuthWrapper.js
@@ -11,9 +11,20 @@ axios.defaults.withCredentials = true
 export default function APIAuthWrapper(props) {
     const [user, setUser] = useState("");
 
-    useEffect(() => {updateUser()}, []);
+    useEffect(() => {
+        updateUser()
+        getCSRFToken() //Needed to get CSRF token first time app is run
+    }, []);
 
     const {children, ...extras} = props;
+
+    // GET request to a valide rendered page by Django to get CSRF token
+    // https://stackoverflow.com/a/75014511/7604434
+    const getCSRFToken = () => {
+        return axios.get(`${endpoint}/login/`, {})
+            // .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+    };
 
     const updateUser = () => {
         return axios.get(`${endpoint}/get-user/`, {
